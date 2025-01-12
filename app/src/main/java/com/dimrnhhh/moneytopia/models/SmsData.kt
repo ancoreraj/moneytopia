e89aad39e60ec58a707d9196bd539c08cec833a8
Acer <ankur.jar123@gmail.com>
2025-01-12 15:49:26 +0530

    SMS ingest

---
+++ app/src/main/java/com/dimrnhhh/moneytopia/models/SmsData.kt
@@ -0,0 +1,9 @@
+package com.dimrnhhh.moneytopia.models
+
+data class SmsData (
+    val id: String,
+    val address: String,
+    val body: String,
+    val date: String,
+    val dateSent: String,
+)
