24e58887ccc2afea15c9fc0a4c8f8d6ed0c4b660
tanmayshree <tanmay.25ts@gmail.com>
2025-04-03 14:00:58 +0530

    added serializer and parcelable dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.io.realm.kotlin) apply false
     alias(libs.plugins.compose.compiler)
+    kotlin("plugin.serialization") version "1.9.0" apply false
 }
